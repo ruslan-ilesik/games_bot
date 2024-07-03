6679361e25d06414a3feb36fb77f515876edc773
ruslan-ilesik <ilesik@lesikr.com>
2024-07-03 19:18:43 +0200

    documenting modules and modules_manager

--- src/module/module.hpp
+++ src/module/module.hpp
@@ -3,48 +3,106 @@
 //
 
 #pragma once
+
 #include <string>
 #include <memory>
 #include <vector>
 #include <map>
+
 namespace gb {
 
+    /**
+     * @brief Forward declaration of the Module class.
+     */
     class Module;
+
+    /**
+     * @brief Shared pointer type alias for Module.
+     */
     typedef std::shared_ptr<Module> Module_ptr;
-    typedef std::map<std::string,Module_ptr> Modules;
+
+    /**
+     * @brief Map type alias for mapping module names to Module pointers.
+     */
+    typedef std::map<std::string, Module_ptr> Modules;
+
+    /**
+     * @brief Function pointer type alias for creating Module instances.
+     */
     typedef Module_ptr (*create_func_t)();
 
-
+    /**
+     * @brief Exception class for methods that are not implemented.
+     */
     class Not_implemented_exception : public std::logic_error
     {
     public:
+        /**
+         * @brief Constructor for Not_implemented_exception.
+         * @param error Error message indicating the method is not implemented.
+         */
         explicit Not_implemented_exception(const std::string& error = "Method is not implemented");
     };
 
+    /**
+     * @brief Base class for modules.
+     */
     class Module {
     protected:
+        /**
+         * @brief Name of the module.
+         */
         std::string _name;
+
+        /**
+         * @brief Dependencies of the module: names of the modules this module depends on.
+         */
         std::vector<std::string> _dependencies;
 
-        explicit Module(const std::string& name, const  std::vector<std::string>& _dependencies = {});
-    public:
+        /**
+         * @brief Protected constructor for Module.
+         * @param name Name of the module.
+         * @param dependencies List of dependencies for the module.
+         */
+        explicit Module(const std::string& name, const std::vector<std::string>& dependencies = {});
 
+    public:
+        /**
+         * @brief Virtual destructor for Module.
+         */
         virtual ~Module() = default;
 
+        /**
+         * @brief Get the name of the module.
+         * @return Name of the module.
+         */
         std::string get_name();
 
+        /**
+         * @brief Get the dependencies of the module.
+         * @return List of dependencies of the module.
+         */
         std::vector<std::string> get_dependencies();
 
-        //below are methods which should be implemented by every module
-        //Note that module manager is always passed here;
+        /**
+         * @brief Run method to be implemented by subclasses.
+         * @param modules Map of all modules managed by the application, including the module manager as "module_manager".
+         *        Any additional dependencies required by the module should also be included in this map.
+         * @note This method must be overridden by subclasses to define module behavior.
+         */
         virtual void run(const Modules& modules);
 
+        /**
+         * @brief Stop method to be implemented by subclasses.
+         * @note This method must be overridden by subclasses to define module stopping behavior.
+         */
         virtual void stop();
-
-
     };
 
+    /**
+     * @brief External C function declaration to create a Module instance.
+     * @return Shared pointer to a new Module instance.
+     */
     extern "C" Module_ptr create();
 }
-
 
--- src/module/modules_manager.hpp
+++ src/module/modules_manager.hpp
@@ -13,90 +13,263 @@
 #include "../utils/filewatch/FileWatch.hpp"
 
 namespace gb {
-    //Note that module manager is always passed to modules;
+
+    /**
+     * @brief Forward declaration of Modules_manager class.
+     */
     class Modules_manager;
 
+    /**
+     * @brief Shared pointer type alias for Modules_manager.
+     */
     typedef std::shared_ptr<Modules_manager> Modules_manager_ptr;
 
+    /**
+     * @brief Represents a manager for handling dynamic modules.
+     * Inherits from Module and enables shared ownership using std::enable_shared_from_this.
+     */
     class Modules_manager : public Module, public std::enable_shared_from_this<Module> {
     private:
+        /**
+         * @brief  Forward declaration of class representing an internal module managed by Modules_manager.
+         */
         class Internal_module;
 
+        /**
+         * @brief Map type alias for mapping module names to Internal_module instances.
+         */
         typedef std::map<std::string, Internal_module> Internal_modules;
 
+        /**
+         * @brief Represents an internal module loaded dynamically.
+         */
         class Internal_module {
         private:
+
+            /**
+             * @brief Handler for the loaded dynamic library.
+             */
             void *_library_handler;
+
+            /**
+             * @brief Pointer to the loaded Module instance.
+             */
             Module_ptr _module;
+
+            /**
+             * @brief List of module names which are dependent on this module.
+             */
             std::vector<std::string> _module_dependent;
+
+            /**
+             * @brief File path of the dynamic library.
+             */
             std::string _file_path;
+
+            /**
+             * @brief Flag indicating if the module is running.
+             */
             bool _is_running = false;
+
         public:
-            Internal_module(void *library_handler, const Module_ptr &module, const std::string& path = "",
+            /**
+             * @brief Constructor for Internal_module.
+             * @param library_handler Handler for the loaded dynamic library.
+             * @param module Pointer to the loaded Module instance.
+             * @param path File path of the dynamic library.
+             * @param module_dependent List of modules which depends on this module.
+             */
+            Internal_module(void *library_handler, const Module_ptr &module, const std::string &path = "",
                             const std::vector<std::string> &module_dependent = {});
 
+            /**
+             * @brief Get the handler for the loaded dynamic library.
+             * @return Handler for the loaded dynamic library.
+             */
             void *get_library_handler() const;
 
+            /**
+             * @brief Get the pointer to the loaded Module instance.
+             * @return Pointer to the loaded Module instance.
+             */
             Module_ptr get_module() const;
 
+            /**
+             * @brief Get the list of module names dependent on this module.
+             * @return List of module names dependent on this module.
+             */
             std::vector<std::string> get_module_dependent() const;
 
+            /**
+             * @brief Add a module name to the list of dependent on this module.
+             * @param name Module name to add as a dependent on this module.
+             * @note it is called by modules_manger when some depndent module gets loaded.
+             */
             void add_module_dependent(const std::string &name);
 
+            /**
+             * @brief Remove a module name from the list of dependent on this module.
+             * @param name Module name to remove from dependent on this module.
+             * @note it is called by modules_manger when some depndent module gets closed.
+             */
             void remove_module_dependent(const std::string &name);
 
+            /**
+             * @brief Check if the module is currently running.
+             * @return True if the module is running, false otherwise.
+             */
             bool is_running() const;
 
+            /**
+             * @brief Check if the module has sufficient dependencies fulfilled.
+             * @param modules Map of internal modules managed by Modules_manager.
+             * @return True if the module has sufficient dependencies, false otherwise.
+             */
             bool has_sufficient_dependencies(const Internal_modules &modules);
 
+            /**
+             * @brief Run method to execute the module's functionality.
+             * @param modules Map of internal modules managed by Modules_manager.
+             */
             void run(const Internal_modules &modules);
 
+            /**
+             * @brief Get the file path of the dynamic library.
+             * @return File path of the dynamic library.
+             */
             std::string get_file_path();
 
+            /**
+             * @brief Stop method to stop the module.
+             */
             void stop();
         };
 
+        /**
+         * @brief Mutex for thread-safe access to internal data structures.
+         */
         std::shared_mutex _mutex;
+
+        /**
+         * @brief File watcher for monitoring module file changes.
+         */
         filewatch::FileWatch<std::string> _watch;
+
+        /**
+         * @brief Map of loaded internal modules.
+        */
         std::map<std::string, Internal_module> _modules;
+
+        /**
+          * @brief Path to the directory containing module files.
+         */
         std::filesystem::path _modules_path;
+
+        /**
+         * @brief Flag indicating whether module loading is allowed.
+         * @note Does not impact modules stopping.
+        */
         bool _allow_modules_load = true;
 
-        Module_ptr getptr();
-
-        void do_stop_module(const std::string &name);
-
-        void do_run_module(const std::string &name);
-
-        std::string do_load_module(const std::string &name);
-
-    protected:
+        /**
+         * @brief Private constructor for Modules_manager.
+         * @param modules_path Path to the directory containing module files.
+         */
         explicit Modules_manager(const std::filesystem::path &modules_path);
 
     public:
+        /**
+         * @brief Factory method to create an instance of Modules_manager.
+         * @param modules_path Path to the directory containing module files. Default is current directory.
+         * @return Shared pointer to the created Modules_manager instance.
+         */
         static Modules_manager_ptr create(const std::filesystem::path &modules_path = {"./"});
 
-        //do not use yourself, only for internal use
-        virtual void run(const Modules &modules);
+        /**
+         * @brief Virtual destructor for Modules_manager.
+         */
+        virtual ~Modules_manager() = default;
 
-        std::string load_module(const std::string &name);
-
-        //RUNS ALL THE MODULES;
-        void run_modules();
-
-        void run_module(const std::string &name);
-
-        // STOPS ALL THE MODULES
-        void stop_modules();
-
-        void stop_module(const std::string &name);
-
+        /**
+         * @brief Run method to execute all loaded modules.
+         */
         void run();
 
+        /**
+        * @brief Run method to execute all loaded modules.
+        */
+        void run_modules();
+
+        /**
+         * @brief Run method to execute a specific module by name.
+         * @param name Name of the module to run.
+         */
+        void run_module(const std::string &name);
+
+        /**
+         * @brief Stop method to stop all running modules and dealocate them.
+         */
+        void stop_modules();
+
+        /**
+         * @brief Stop method to stop a specific module by name.
+         * @param name Name of the module to stop and dealocate.
+         */
+        void stop_module(const std::string &name);
+
+        /**
+         * @brief Load a module dynamically from the specified file path.
+         * @param path path of the module to load.
+         * @return Name of the loaded module.
+         */
+        std::string load_module(const std::string &path);
+
+        /**
+         * @brief Set whether module loading is allowed.
+         * @param v True to allow module loading, false otherwise.
+         */
         void set_allow_modules_load(bool v);
 
+        /**
+         * @brief Check if module loading is allowed.
+         * @return True if module loading is allowed, false otherwise.
+         */
         bool is_allowed_modules_load();
 
-    };
-}
+    protected:
+        /**
+         * @brief Run method to be overridden by subclasses.
+         * @param modules Map of all modules managed by the application, including the module manager as "module_manager".
+         *        Any additional dependencies required by the module should also be included in this map.
+         * @note This method must be overridden by subclasses to define module behavior.
+         */
+        virtual void run(const Modules &modules) override;
 
+    private:
+        /**
+         * @brief Get a shared pointer to this Modules_manager instance.
+         * @return Shared pointer to this Modules_manager instance.
+         */
+        Module_ptr getptr();
+
+        /**
+         * @brief Internal method to stop a module by name.
+         * @param name Name of the module to stop.
+         */
+        void do_stop_module(const std::string &name);
+
+        /**
+         * @brief Internal method to run a module by name.
+         * @param name Name of the module to run.
+         */
+        void do_run_module(const std::string &name);
+
+        /**
+         * @brief Internal method to load a module dynamically from the specified file path.
+         * @param file path of the module to load.
+         * @return Name of the loaded module.
+         */
+        std::string do_load_module(const std::string &path);
+    };
+
+}
