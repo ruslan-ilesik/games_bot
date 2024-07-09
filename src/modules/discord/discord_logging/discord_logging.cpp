54962f549ea637213b8f940872c6673c268894cd
ruslan-ilesik <ruslan@ilesik.com>
2024-07-09 22:54:48 +0200

    small fix of runtime reload behaviour of discord_logging module

--- src/modules/discord/discord_logging/discord_logging.cpp
+++ src/modules/discord/discord_logging/discord_logging.cpp
@@ -18,32 +18,39 @@ namespace gb {
     void Discord_logging::innit(const Modules &modules) {
         this->_discord_bot = std::static_pointer_cast<Discord_bot>(modules.at("discord_bot"));
         this->_log = std::static_pointer_cast<Logging>(modules.at("logging"));
-        _discord_bot->pre_requirements.push_back([this]() {
-            _bot_log_handle = _discord_bot->get_bot()->on_log([this](const dpp::log_t &event) {
-                std::string message = "Discord bot: " + event.message;
-                switch (event.severity) {
-                    case dpp::ll_trace:
-                        _log->trace(message);
-                        break;
-                    case dpp::ll_debug:
-                        _log->debug(message);
-                        break;
-                    case dpp::ll_info:
-                        _log->info(message);
-                        break;
-                    case dpp::ll_warning:
-                        _log->warn(message);
-                        break;
-                    case dpp::ll_error:
-                        _log->error(message);
-                        break;
-                    case dpp::ll_critical:
-                    default:
-                        _log->critical(message);
-                        break;
-                }
+        auto log_fn = [this](const dpp::log_t &event) {
+            std::string message = "Discord bot: " + event.message;
+            switch (event.severity) {
+                case dpp::ll_trace:
+                    _log->trace(message);
+                    break;
+                case dpp::ll_debug:
+                    _log->debug(message);
+                    break;
+                case dpp::ll_info:
+                    _log->info(message);
+                    break;
+                case dpp::ll_warning:
+                    _log->warn(message);
+                    break;
+                case dpp::ll_error:
+                    _log->error(message);
+                    break;
+                case dpp::ll_critical:
+                default:
+                    _log->critical(message);
+                    break;
+            }
+        };
+        if (_discord_bot->get_bot() == nullptr){
+            _discord_bot->pre_requirements.emplace_back([this,log_fn]() {
+
             });
-        });
+        }
+        else{
+            _bot_log_handle = _discord_bot->get_bot()->on_log(log_fn);
+        };
+
     }
 
     Module_ptr create() {
